3483f915ff2d15a0a2a76adaf623bf1fdb0b7561
AndrewQuijano <andrewquijano92@gmail.com>
2016-07-30 22:44:32 -0400

    Add files via upload

---
+++ CSCI212/Homework5/Node.java
@@ -0,0 +1,22 @@
+//September 21
+public class Node 
+		{ 
+		//Dynamic Data Structure
+		//Because this can change.
+				private int data; 
+				private Node next; 
+		//We use instance variables NOT static 
+		//Because there are many Nodes.
+				public Node(int d) //Crate Node
+				{
+					data = d; //Place integer value in the Object
+				}
+				public Node(int d, Node n) //Create Node
+				{
+					data = d; //Value inside the Object
+					next = n; //Point to the Next Node
+				}
+				public int getData(){return data;} //Get Value
+				public Node getNext(){return next;} //Get the Next Node Object
+				public void setNext(Node n){next=n;} //Change the Pointer
+		}
