ba1b88410aea7d9ead36e28d41355e2a18806958
AndrewQuijano <andrewquijano92@gmail.com>
2016-07-30 22:03:35 -0400

    Add files via upload

---
+++ CSCI211/WaxmanHW7.cpp
@@ -0,0 +1,72 @@
+/*
+ * WaxmanHW7.cpp
+ *
+ *  Created on: Feb 18, 2016
+ *      Author: andrew
+ *      1D Array 8 Queens with 1D array WITHOUT goto
+ */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+bool ok (int board []) //Check if it is valid location to place a Queen
+{
+	for (int i=7;i>=0;i--)
+	{
+		for (int j=0;j<i;j++)
+		{
+			if (board[i]==board[j]||(i-j)==abs(board[i]-board[j]))
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void print (int board [])
+{
+	static int count=1;
+	cout << "Solution #" << count++ << ":" << endl;
+	for (int i=0;i<8;++i)
+	{
+		cout << board[i] << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int board[8]={0};
+	for (board[0]=0;board[0]<8;board[0]++)
+	{
+		for (board[1]=0;board[1]<8;board[1]++)
+		{
+			for (board[2]=0;board[2]<8;board[2]++)
+			{
+				for (board[3]=0;board[3]<8;board[3]++)
+				{
+					for (board[4]=0;board[4]<8;board[4]++)
+					{
+						for (board[5]=0;board[5]<8;board[5]++)
+						{
+							for (board[6]=0;board[6]<8;board[6]++)
+							{
+								for (board[7]=0;board[7]<8;board[7]++)
+								{
+									if(ok(board))
+									{
+											print(board);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ CSCI211/WaxmanHW8.cpp
@@ -0,0 +1,98 @@
+/*
+ * WaxmanHW8.cpp
+ *
+ *  Created on: Feb 25, 2016
+ *      Author: andrew
+ *      Homework 8
+ *		8 numbers in a cross problem
+ *		Write a program which allocates the integers 1-8 to the squares in the figure above, subject to
+the restrictions that no two adjacent squares contain consecutive integers_ _
+ _|1|2|_
+|0|3|4|7|
+  |5|6|
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+void backtrack(int &square)
+{
+    square--;
+    if(square==-1)
+	{
+    	exit(0);
+    }
+}
+
+bool ok(int q[8], int square)
+{
+    int adjacents[8][4]={{-1, -1, -1, -1},
+                        {0, -1, -1, -1},
+                        {1, -1, -1, -1},
+                        {0, 1, 2, -1},
+                        {1, 2, 3, -1},
+                        {0, 3, 4, -1},
+                        {3, 4, 5, -1},
+                        {2, 4, 6, -1}};
+    for(int i=0; i<square; i++)
+    {
+        if(q[i]==q[square])
+        {
+        	return false;
+        }
+    }
+    for(int j=0; adjacents[square][j]!=-1; j++)
+    {
+        if(abs(q[square]-q[adjacents[square][j]])==1)
+        {
+        	return false;
+        }
+    }
+    return true;
+}
+
+void print(int q[8])
+{
+    cout << endl;
+    cout << "  " << q[1] << " " << q[2] << endl;
+    cout << q[0] << " " << q[3] << " " << q[4] << " " << q[7] << endl;
+    cout << "  " << q[5] << " " << q[6] << endl;
+}
+
+int main()
+{
+    int Solution[8]={0}, square=0;
+    bool from_backtrack=false;
+
+    while(true)
+    {
+        while(square<8)
+        {
+        	if(!from_backtrack)
+        	{
+        		Solution[square]=0;
+        	}
+
+            while(Solution[square]<9)
+            {
+            	Solution[square]++;
+                if(Solution[square]==9)
+                {
+                    backtrack(square);
+                    continue;
+                }
+                if(ok(Solution,square))
+                {
+                	break;
+                }
+            }
+            square++;
+            from_backtrack=false;
+        }
+        print(Solution);
+        backtrack(square);
+        from_backtrack=true;
+    }
+    return 0;
+}
