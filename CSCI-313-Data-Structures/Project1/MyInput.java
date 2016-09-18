6d48123d2b006eab7bb320d50c7dc76ae1606052
Andrew <andrewquijano92@gmail.com>
2016-09-18 13:47:05 -0400

    Add files via upload

--- CSCI-313-Data-Structures/Project1/MyInput.java
+++ CSCI-313-Data-Structures/Project1/MyInput.java
@@ -5,6 +5,7 @@ public class MyInput
 	{
 		LLSparseM lion= new LLSparseM(1,10);//Row/Column
 		lion.setElement(1, 2, 4);//Row,Column,Value
+		lion.setElement(1, 2, 30);//NPE!!!!!!!!!!!!!
 		System.out.println(lion.getRowHead().getRowID());
 		System.out.println(lion.getColumnHead().getColumn());
 		System.out.println(lion.getColumnHead().getNextRow().getValue());
