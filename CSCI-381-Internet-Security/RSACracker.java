a41a4808b9e0af6a52b647d12e4fa65e0ce05d34
Andrew <andrewquijano92@gmail.com>
2017-02-26 18:51:07 -0500

    Add files via upload

--- CSCI-381-Internet-Security/RSACracker.java
+++ CSCI-381-Internet-Security/RSACracker.java
@@ -1,18 +1,103 @@
 import java.util.ArrayList;
-import java.math.*;;
 
 public class RSACracker
 {
+	int decoder=-1;
+	int encoder=0;
+	int bigPrime=0;
+	ArrayList<Integer> smallPrimes;
+	
+	public RSACracker(int N, int e)
+	{
+		encoder=e;
+		bigPrime=N;
+		smallPrimes=FermatFactorization(bigPrime);
+	}
+	
+	public int getDecoder()
+	{
+		if (decoder==-1)
+		{
+			decoder=RSACrackDecoder(smallPrimes.get(0),smallPrimes.get(1),encoder);
+			return decoder;
+		}
+		else
+		{
+			return decoder;
+		}
+	}
+	
 	public int RSACrackDecoder(int p, int q, int e)
 	{
-		int decoder=0;
 		int LCM = LCM (p-1,q-1);
 		//Formula to crack decoder:
 		//de CONGRUENT to 1(Mod LCM(p-1,q-1))
 		//Linear Combination:
 		// e*x + LCM*y = 1
+		ArrayList <Integer> linearCombo = extendedGCD(e, LCM);
+		//Note for exGCD to work
+		//The larger coordinate HAS to be on exGCDMatrix[0][2];
+		//So which element my return depends on whether encoder or LCM is bigger
+		//In my case, I need the decoder...
+		if (e > LCM)
+		{
+			return linearCombo.get(0);
+		}
+		return linearCombo.get(1);
+		
+	}
 	
-		return decoder;
+	public ArrayList<Integer> extendedGCD(int x, int y)
+	{
+		ArrayList<Integer> solution = new ArrayList<Integer>();
+		int [][] exGCDMatrix = new int [2][3];
+		exGCDMatrix [0][0]=1;
+		exGCDMatrix [0][1]=0;
+		exGCDMatrix [1][0]=0;
+		exGCDMatrix [1][1]=1;
+		
+		if (x > y)
+		{
+			exGCDMatrix [0][2]=x;
+			exGCDMatrix [1][2]=y;
+		}
+		else if (x==y)
+		{
+			solution.add(1);
+			solution.add(-1);
+			return solution;
+		}
+		else
+		{
+			exGCDMatrix [0][2]=y;
+			exGCDMatrix [1][2]=x;
+		}
+		
+		int quotient;		
+		int [] Row2temp = new int [3];
+		
+		while (true)
+		{
+			if (exGCDMatrix[1][2]==0)
+			{
+				solution.add(exGCDMatrix[0][0]);
+				solution.add(exGCDMatrix[0][1]);
+				return solution;
+			}
+			//Step 1: Store value of Row 2 in Temporary row.
+			Row2temp[0]=exGCDMatrix[1][0];
+			Row2temp[1]=exGCDMatrix[1][1];
+			Row2temp[2]=exGCDMatrix[1][2];
+			//Step 2: compute value of new Row 2.
+			quotient = exGCDMatrix[0][2]/exGCDMatrix[1][2];
+			exGCDMatrix[1][0]=exGCDMatrix[0][0]-(quotient*exGCDMatrix[1][0]);
+			exGCDMatrix[1][1]=exGCDMatrix[0][1]-(quotient*exGCDMatrix[1][1]);
+			exGCDMatrix[1][2]=exGCDMatrix[0][2]-(quotient*exGCDMatrix[1][2]);
+			//Step 3: Move old Row 2 to Row 1.
+			Row2temp[0]=exGCDMatrix[0][0];
+			Row2temp[1]=exGCDMatrix[0][1];
+			Row2temp[2]=exGCDMatrix[0][2];
+		}
 	}
 	
 	public ArrayList<Integer> FermatFactorization (int N)
